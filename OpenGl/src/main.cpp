f16fb81a74ec732917281e2efde5b0a2e7d08a4f
fxxsx <nineteen.yt123@gmail.com>
2022-09-05 21:26:12 +0200

    Start working with vertex buffers

--- OpenGl/src/main.cpp
+++ OpenGl/src/main.cpp
@@ -27,18 +27,25 @@ int main(void)
     if (glewInit() != GLEW_OK)
         return -1;
 
+    float positions[6] = {
+        0.0f, 0.5f
+        ,0.5f, -0.5f
+        ,-0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glColor3f(255, 215, 0);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glVertex2f(-0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
