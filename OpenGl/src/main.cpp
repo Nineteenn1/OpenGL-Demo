86cb0600826bf29ab18d11099a744055f440adb7
fxxsx <nineteen.yt123@gmail.com>
2022-09-13 19:05:35 +0200

    Setup imgui (not working yet)

--- OpenGl/src/main.cpp
+++ OpenGl/src/main.cpp
@@ -1,5 +1,11 @@
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 
 #include <iostream>
 #include <string>
@@ -69,6 +75,16 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+
+    ImGui_ImplGlfw3_Init(window, true);
 
     if (glewInit() != GLEW_OK)
         return -1;
