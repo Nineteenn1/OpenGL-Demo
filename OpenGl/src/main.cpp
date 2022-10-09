f62782dcd60a667666684a0a0d2e22585b44e099
fxxsx <nineteen.yt123@gmail.com>
2022-10-09 18:27:22 +0200

    Update gitignore

--- OpenGl/src/main.cpp
+++ OpenGl/src/main.cpp
@@ -64,16 +64,6 @@ float triangleVertices[6] = {
 	,-0.4f, -0.4f,
 };
 
-//float squareVertices[12] = {
-//	-0.4f, -0.4f
-//	,0.4f, -0.4f
-//	,0.4f, 0.4f
-//
-//	,0.4f, 0.4f
-//	,-0.4f, 0.4f
-//	,-0.4f, -0.4f
-//};
-
 float squareVertices[12] = {
 	200.0f, 200.0f
 	,400.0f, 200.0f
@@ -102,9 +92,6 @@ float bgcolor[4] = {
 	0, 0, 0, 0
 };
 
-float scale[3] = {
-	1.0f, 1.0f, 1.0f
-};
 
 
 bool drawUIElements = true;
@@ -214,6 +201,67 @@ int main( void )
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
 
+
+	// style copied from https://github.com/ocornut/imgui/issues/707#issuecomment-678611331
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.Colors[ImGuiCol_Text] = ImVec4( 1.00f, 1.00f, 1.00f, 1.00f );
+	style.Colors[ImGuiCol_TextDisabled] = ImVec4( 0.50f, 0.50f, 0.50f, 1.00f );
+	style.Colors[ImGuiCol_WindowBg] = ImVec4( 0.13f, 0.14f, 0.15f, 1.00f );
+	style.Colors[ImGuiCol_ChildBg] = ImVec4( 0.13f, 0.14f, 0.15f, 1.00f );
+	style.Colors[ImGuiCol_PopupBg] = ImVec4( 0.13f, 0.14f, 0.15f, 1.00f );
+	style.Colors[ImGuiCol_Border] = ImVec4( 0.43f, 0.43f, 0.50f, 0.50f );
+	style.Colors[ImGuiCol_BorderShadow] = ImVec4( 0.00f, 0.00f, 0.00f, 0.00f );
+	style.Colors[ImGuiCol_FrameBg] = ImVec4( 0.25f, 0.25f, 0.25f, 1.00f );
+	style.Colors[ImGuiCol_FrameBgHovered] = ImVec4( 0.38f, 0.38f, 0.38f, 1.00f );
+	style.Colors[ImGuiCol_FrameBgActive] = ImVec4( 0.67f, 0.67f, 0.67f, 0.39f );
+	style.Colors[ImGuiCol_TitleBg] = ImVec4( 0.08f, 0.08f, 0.09f, 1.00f );
+	style.Colors[ImGuiCol_TitleBgActive] = ImVec4( 0.08f, 0.08f, 0.09f, 1.00f );
+	style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4( 0.00f, 0.00f, 0.00f, 0.51f );
+	style.Colors[ImGuiCol_MenuBarBg] = ImVec4( 0.14f, 0.14f, 0.14f, 1.00f );
+	style.Colors[ImGuiCol_ScrollbarBg] = ImVec4( 0.02f, 0.02f, 0.02f, 0.53f );
+	style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4( 0.31f, 0.31f, 0.31f, 1.00f );
+	style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4( 0.41f, 0.41f, 0.41f, 1.00f );
+	style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4( 0.51f, 0.51f, 0.51f, 1.00f );
+	style.Colors[ImGuiCol_CheckMark] = ImVec4( 0.11f, 0.64f, 0.92f, 1.00f );
+	style.Colors[ImGuiCol_SliderGrab] = ImVec4( 0.11f, 0.64f, 0.92f, 1.00f );
+	style.Colors[ImGuiCol_SliderGrabActive] = ImVec4( 0.08f, 0.50f, 0.72f, 1.00f );
+	style.Colors[ImGuiCol_Button] = ImVec4( 0.25f, 0.25f, 0.25f, 1.00f );
+	style.Colors[ImGuiCol_ButtonHovered] = ImVec4( 0.38f, 0.38f, 0.38f, 1.00f );
+	style.Colors[ImGuiCol_ButtonActive] = ImVec4( 0.67f, 0.67f, 0.67f, 0.39f );
+	style.Colors[ImGuiCol_Header] = ImVec4( 0.22f, 0.22f, 0.22f, 1.00f );
+	style.Colors[ImGuiCol_HeaderHovered] = ImVec4( 0.25f, 0.25f, 0.25f, 1.00f );
+	style.Colors[ImGuiCol_HeaderActive] = ImVec4( 0.67f, 0.67f, 0.67f, 0.39f );
+	style.Colors[ImGuiCol_Separator] = style.Colors[ImGuiCol_Border];
+	style.Colors[ImGuiCol_SeparatorHovered] = ImVec4( 0.41f, 0.42f, 0.44f, 1.00f );
+	style.Colors[ImGuiCol_SeparatorActive] = ImVec4( 0.26f, 0.59f, 0.98f, 0.95f );
+	style.Colors[ImGuiCol_ResizeGrip] = ImVec4( 0.00f, 0.00f, 0.00f, 0.00f );
+	style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4( 0.29f, 0.30f, 0.31f, 0.67f );
+	style.Colors[ImGuiCol_ResizeGripActive] = ImVec4( 0.26f, 0.59f, 0.98f, 0.95f );
+	style.Colors[ImGuiCol_Tab] = ImVec4( 0.08f, 0.08f, 0.09f, 0.83f );
+	style.Colors[ImGuiCol_TabHovered] = ImVec4( 0.33f, 0.34f, 0.36f, 0.83f );
+	style.Colors[ImGuiCol_TabActive] = ImVec4( 0.23f, 0.23f, 0.24f, 1.00f );
+	style.Colors[ImGuiCol_TabUnfocused] = ImVec4( 0.08f, 0.08f, 0.09f, 1.00f );
+	style.Colors[ImGuiCol_TabUnfocusedActive] = ImVec4( 0.13f, 0.14f, 0.15f, 1.00f );
+	style.Colors[ImGuiCol_DockingPreview] = ImVec4( 0.26f, 0.59f, 0.98f, 0.70f );
+	style.Colors[ImGuiCol_DockingEmptyBg] = ImVec4( 0.20f, 0.20f, 0.20f, 1.00f );
+	style.Colors[ImGuiCol_PlotLines] = ImVec4( 0.61f, 0.61f, 0.61f, 1.00f );
+	style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4( 1.00f, 0.43f, 0.35f, 1.00f );
+	style.Colors[ImGuiCol_PlotHistogram] = ImVec4( 0.90f, 0.70f, 0.00f, 1.00f );
+	style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4( 1.00f, 0.60f, 0.00f, 1.00f );
+	style.Colors[ImGuiCol_TextSelectedBg] = ImVec4( 0.26f, 0.59f, 0.98f, 0.35f );
+	style.Colors[ImGuiCol_DragDropTarget] = ImVec4( 0.11f, 0.64f, 0.92f, 1.00f );
+	style.Colors[ImGuiCol_NavHighlight] = ImVec4( 0.26f, 0.59f, 0.98f, 1.00f );
+	style.Colors[ImGuiCol_NavWindowingHighlight] = ImVec4( 1.00f, 1.00f, 1.00f, 0.70f );
+	style.Colors[ImGuiCol_NavWindowingDimBg] = ImVec4( 0.80f, 0.80f, 0.80f, 0.20f );
+	style.Colors[ImGuiCol_ModalWindowDimBg] = ImVec4( 0.80f, 0.80f, 0.80f, 0.35f );
+	style.GrabRounding = style.FrameRounding = 2.3f;
+
+
+	// ------------------------------- // 
+
+	float _scale = 1.0f;
+
 	unsigned int buffer;
 
 	glGenBuffers( 1, &buffer );
@@ -222,7 +270,6 @@ int main( void )
 
 	glEnableVertexAttribArray( 0 );
 	glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( float ) * 2, 0 );
-
 
 	ShaderProgramSource source = ParseShader( "res/shaders/shader.shader" );
 
@@ -233,6 +280,7 @@ int main( void )
 	glBindBuffer( GL_ARRAY_BUFFER, 0 );
 
 	glm::vec3 translation( 0.0f, 0.0f, 0.0f );
+
 	/* Loop until the user closes the window */
 	while ( !glfwWindowShouldClose( window ) )
 	{
@@ -246,6 +294,8 @@ int main( void )
 
 		if ( drawSquare )
 			glDrawArrays( GL_TRIANGLES, 0, 6 );
+		if ( rotateLeft )
+			model = glm::rotate( model, 4.0f, glm::vec3( 1.0f, 0.0f, 0.0f) );
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
@@ -266,8 +316,8 @@ int main( void )
 			{
 				ImGui::ColorEdit4( "Object Color", color );
 				ImGui::ColorEdit4( "Background Color", bgcolor );
-				ImGui::SliderFloat2( "Translate", &translation.x, 0.0f, 1280.0f );
-				ImGui::SliderFloat3( "Scale", scale, -5.0f, 5.0f );
+				ImGui::SliderFloat2( "Translate", &translation.x, 0.0f, 990.0f );
+				ImGui::SliderFloat( "Scale", &_scale, -5.0f, 5.0f );
 			}
 
 			ImGui::BeginChild( "Draw Shapes" );
@@ -283,14 +333,11 @@ int main( void )
 				ImGui::Checkbox( "Draw Cube", &drawCube );
 				ImGui::Checkbox( "Draw Sphere", &drawSphere );
 			}
-			ImGui::EndChild();
 
-			ImGui::BeginChild( "Rotation" );
 			if ( ImGui::CollapsingHeader( "Rotation" ) )
 			{
-				ImGui::Checkbox( "Rotate", &rotate );
-				ImGui::Checkbox( "Rotate Left", &rotateRight );
-				ImGui::Checkbox( "Rotate Right", &rotateLeft );
+				ImGui::Checkbox( "Rotate Left", &rotateLeft );
+				ImGui::Checkbox( "Rotate Right", &rotateRight );
 			}
 			ImGui::EndChild();
 
@@ -323,7 +370,6 @@ int main( void )
 
 		glUseProgram( shader );
 		glUniform4f( glGetUniformLocation( shader, "u_Color" ), color[0], color[1], color[2], color[3] );
-		glUniform4f( glGetUniformLocation( shader, "position" ), position[0], position[1], position[2], position[3] );
 		glUniformMatrix4fv( glGetUniformLocation( shader, "u_MVP" ), 1, GL_FALSE, &mvp[0][0] );
 
 		ImGui::Render();
